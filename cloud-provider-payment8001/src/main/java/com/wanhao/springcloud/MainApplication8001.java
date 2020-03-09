978834926f46d41768273c86ff76c9f97f34921e
liulihao <838088516@qq.com>
2020-03-09 18:05:04 +0800

    基础增删改查代码添加

---
+++ cloud-provider-payment8001/src/main/java/com/wanhao/springcloud/MainApplication8001.java
@@ -0,0 +1,17 @@
+package com.wanhao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by LiuLiHao on 2020/3/8 0008 下午 09:28
+ * @author : LiuLiHao
+ * 描述：启动类
+ */
+@SpringBootApplication
+public class MainApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication8001.class,args);
+    }
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/wanhao/springcloud/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.wanhao.springcloud.controller;
+
+import com.wanhao.springcloud.entities.CommonResult;
+import com.wanhao.springcloud.entities.Payment;
+import com.wanhao.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by LiuLiHao on 2020/3/8 0008 下午 10:22
+ * @author : LiuLiHao
+ * 描述：基础增删改查测试
+ */
+@RestController
+@RequestMapping(value = "payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping(value = "create")
+    public CommonResult<Payment> create(Payment payment){
+        int i = paymentService.create(payment);
+        if (i>0){
+            return new CommonResult<>(CommonResult.success,"操作成功");
+        }
+        return new CommonResult<>(CommonResult.error,"操作失败");
+    }
+
+    @GetMapping(value = "{id}")
+    public CommonResult<Payment> get(@PathVariable("id")Long id){
+        Payment payment = paymentService.getById(id);
+        if (payment!=null){
+            return new CommonResult<>(CommonResult.success,"查询成功",payment);
+        }
+        return new CommonResult<>(CommonResult.error,"没有记录");
+    }
+}
